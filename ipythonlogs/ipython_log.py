5568a0568ccb67cc58a720855403a06ff963a0a4
tylerc-atx <tylerc-atx@github.com>
2017-01-27 17:22:09 -0600

    added exercise 23

---
+++ ipythonlogs/ipython_log.py
@@ -0,0 +1,69 @@
+# IPython log file
+
+from math import *
+sqrt(20)
+(10 **  1/2) * 2
+(10 ** 1/2) * 2
+(10 ** 1/2) * 2
+(5 ** 1/2) * 2
+(10 ** (1/2)) * 2
+sqrt(10) * 2
+from __future__ import division
+(10 ** (1/2)) * 2
+sqrt(20)
+10*sqrt(2)
+sqrt(200)
+10 * sqrt(2)
+19/20
+19*5
+95+4
+99/20
+get_ipython().magic(u'save log.py 1-18')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'save ipythonlogs/log21_22.py 1-20')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'mkdir ipythonlogs')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'save ipythonlogs/log21_22.py 1-20')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'ls ipythonlogs/')
+get_ipython().magic(u'ls ipythonlogs')
+get_ipython().magic(u'ls ')
+rm log.py
+get_ipython().magic(u'ls ')
+rm log.py
+get_ipython().system(u'rm log.py')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'cd ipythonlogs')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'logstart')
+get_ipython().system(u'rm log21_22.py')
+get_ipython().magic(u'ls ')
+19/20
+1000/100000
+factorial(100000)/(factorial(99998)*factorial(2)) * (0.01 ** 2) * ((1 - 0.01) ** 99998)
+factorial(100000)/(factorial(99998)*factorial(2)) * (0.01 ** 2) * ((1 - 0.01000000000000) ** 99998)
+factorial (100000)
+get_ipython().magic(u'ls ')
+clear
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'logon')
+get_ipython().magic(u'cd ..')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'cd ipythonlog')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'cd ipythonlogs')
+get_ipython().magic(u'ls ')
+factorial(100000)/(factorial(99992)*factorial(8)) * (0.01 ** 99992) * ((1 - 0.01000000000000) ** 8)
+long(factorial(100000)/(factorial(99992)*factorial(8)) * (0.01 ** 99992) * ((1 - 0.01000000000000) ** 8))
+float(factorial(100000)/(factorial(99992)*factorial(8)) * (0.01 ** 99992) * ((1 - 0.01000000000000) ** 8))
+45*(.99**8)*(.01**2)
+factorial(0)
+.99**10
+10*(.01**1)*(.99**9)
+0.904382+0.09135
+0.904382+0.0913517
+1-.9957337
+sqrt(2)
+sqrt(20)
+exit()
