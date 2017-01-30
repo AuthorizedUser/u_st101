dc2c52959330137c9d66b0e3d71a7717c4a7b9f4
tylerc-atx <tylerc-atx@github.com>
2017-01-29 23:23:38 -0600

    added ex28

---
+++ L28_1.py
@@ -0,0 +1,19 @@
+#Write
+from math import sqrt
+from __future__ import division
+
+def mean(l):
+    return float(sum(l))/len(l)
+
+def var(l):
+    m = mean(l)
+    return sum([(x-m)**2 for x in l])/len(l)
+
+def factor(l):
+    return 1.96
+
+
+def conf(l):
+    """Takes a set of data and computes the confidence interval"""
+
+    return factor(l) * sqrt(var(l)/len(l))
